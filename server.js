1b662881efd0c3f8bddeb927dd060dd09af92627
soup <z.fike@eagles.oc.edu>
2023-09-03 14:26:16 -0500

    Modified and added backend services

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
-// require("./app/routes/course.routes.js")(app);
+require("./app/routes/course.routes.js")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3011;
